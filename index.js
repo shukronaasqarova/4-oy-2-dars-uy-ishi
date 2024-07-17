4ffab9d1d0dba72cf9c25ac5804179f219fa5e98
shukronaasqarova <shukronaasqarova94@gmail.com>
2024-07-17 23:20:41 +0500

    end

---
+++ index.js
@@ -0,0 +1,292 @@
+// if1
+
+// let a = +prompt("sonni kiriting");
+// if (a > 0){
+//     a++;
+// }
+// console.log(a);
+
+// if2
+
+// let a = +prompt("sonni kiriting");
+// if (a > 0){
+//     a++;
+// } else {
+//     a-=2;
+// }
+// console.log(a);
+
+// if3
+
+// let num = +prompt ('sonni kiriting');
+// if (num>0){
+//     num++;
+// } else if(num==0){
+//     num+=10;
+// } else {
+//     num -=2;
+// }
+// console.log (num);
+
+// if4
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let counter = 0;
+// if (num1 > 0) {
+//   counter++;
+// } if (num2 > 0) {
+//   counter++;
+// } if (num3 > 0) {
+//   counter++;
+// }
+// console.log(counter);
+
+// if5
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+// let counter = 0;
+// let sum = 0;
+// if (num1 > 0) {
+//   counter++;
+// } else {
+//   sum++;
+// }
+// if (num2 > 0) {
+//   counter++;
+// } else {
+//   sum++;
+// }
+// if (num3 > 0) {
+//   counter++;
+// } else {
+//   sum++;
+// }
+// console.log("musbatlar", counter, "ta" );
+// console.log("manfiyler", sum, "ta");
+
+// if6
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let res = "";
+// if (num1 > num2){
+//     res = num1;
+// } else{
+//     res= num2; 
+// }
+// console.log("kattasi", res);
+
+// if7
+
+// let num1 = +prompt ('1 sonni kiriting');
+// let num2 = +prompt ('2 sonni kiriting'); 
+// let small = "";
+// if (num1<num2){
+//     small = num1;    
+// } else{
+//     small = num2;
+// }
+// console.log(small);
+
+// if8
+
+// let num1 = +prompt ('1 sonni kiriting');
+// let num2 = +prompt ('2 sonni kiriting');
+// if (num1>num2){
+//     console.log(num1);    
+// } else{
+//     console.log(num2);
+// }  if (num1<num2){
+//     console.log(num1);    
+// } else{
+//     console.log(num2);
+// } 
+
+// if9 
+
+// let a = +prompt ('a ni kiriting');
+// let b = +prompt ('b ni kiriting'); 
+// if(a>b){
+// b+=a;
+// }
+// console.log(a );
+// console.log(b );
+
+// if10
+
+// let a = +prompt ('a ni kiriting');
+// let b = +prompt ('b ni kiriting'); 
+// let c = "";
+// if (a !== b){
+//     c = a + b;
+//     console.log(c);
+// } if (a == b){
+//     c = 0;
+// }
+// console.log(0);
+// console.log(a);
+// console.log(b);
+
+// if11
+
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3 = "";
+// if (num1==num2){
+//     console.log(0);
+// } if (num1>num2) {
+//     console.log(num1);
+// }  if (num1<num2) {
+//     console.log(num2);
+// }
+
+// if12
+
+// let num1 = +prompt ('num1');
+// let num2 = +prompt ('num2'); 
+// let num3 = +prompt ('num3'); 
+// let res = "";
+// if(num1 < num2){
+//     res = num1;
+// } if (num2 < num3){
+//     res = num2;
+// } if (num3 > res){
+//     res = num3;
+// }
+// console.log(res);
+
+// if13
+
+// if14
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+// let c = +prompt('c ni kiriting');
+// let kichik = 0;
+// let kotta = 0;
+// if (a<b && a<c) {
+//   kichik = a
+// }else if (a>b && a>c) {
+//     kotta = a;
+// }
+
+// if (b<a && b<c) {
+//     kichik = b
+// }else if (b>a && b>c) {
+//     kotta = b;
+// }
+
+// if (c<a && c<b) {
+//     kichik = c;
+// }else if (c>a && c>b) {
+//     kotta = c;
+// }
+
+// console.log(kichik);
+// console.log(kotta);
+
+//----BOOLEAN----
+
+// bolean1
+
+// let a = +prompt("a ni kiriting");
+// if(a > 0){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// boolean2
+
+// let a = +prompt("a ni kiriting");
+// if(a % 2 == 1){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// boolean3
+
+// let a = +prompt("a ni kiriting");
+// if(a % 2 == 0){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// boolean4
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// if (a > 2 && b <=3) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean5
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// if (a >= 0 || b < -2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean6
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// let c  = +prompt ('c ni kiriting');
+// if (a < b && b<=c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }  
+
+// boolean7
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// let c  = +prompt ('c ni kiriting');
+// if (a <= b && b <= c || a >= c) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean8
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// if (a % 2 == 1 && b % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean9
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// if (a % 2 == 1 || b % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
+// boolean10
+
+// let a = +prompt ("a ni kiriting");
+// let b  = +prompt ('b ni kiriting');
+// if (a % 2 == 1 && b % 2 == 0|| a % 2 == 0 && b % 2 == 1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
+
